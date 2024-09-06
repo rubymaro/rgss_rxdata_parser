0a142cd58bc40a4172d38c3f824210cbf1bc4f7e
Jubin Park <jubin-park@naver.com>
2024-09-07 01:39:33 +0900

    Update Main.cpp

--- rgss_rxdata_parser/Main.cpp
+++ rgss_rxdata_parser/Main.cpp
@@ -2,5 +2,40 @@
 
 int wmain(const int argc, const wchar_t* argv[])
 {
-	return 0;
+	FILE* pFile = nullptr;
+	errno_t err;
+	long size;
+	char* paBuf;
+	size_t readByte;
+
+	do
+	{
+		err = _wfopen_s(&pFile, L"Actors.rxdata", L"rb");
+		if (err != 0 || pFile == nullptr)
+		{
+			break;
+		}
+
+		fseek(pFile, 0, SEEK_END);
+		size = ftell(pFile);
+		fseek(pFile, 0, SEEK_SET);
+
+		paBuf = new char[size];
+
+		readByte = fread_s(paBuf, size, sizeof(char), size, pFile);
+
+		delete[] paBuf;
+
+		
+
+		fclose(pFile);
+
+		return 0;
+
+
+	} while (0);
+
+
+
+	return 1;
 }
