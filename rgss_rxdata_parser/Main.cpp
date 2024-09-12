de02823385b8a163c1c82dd690624b8bce859702
Jubin Park <jubin-park@naver.com>
2024-09-12 09:56:28 +0900

    Update Main.cpp

--- rgss_rxdata_parser/Main.cpp
+++ rgss_rxdata_parser/Main.cpp
@@ -11,7 +11,7 @@ void OpenFile(const wchar_t* const pWcsfileName)
 	FILE* pFile = nullptr;
 	errno_t err;
 	unsigned int bufSize;
-	unsigned int  readByte;
+	unsigned int readByte;
 	unsigned char* paBuf = nullptr;
 
 	do
@@ -50,72 +50,73 @@ bool ProcessFixnum(const unsigned char** ppToken, int* outVal)
 	++(*ppToken);
 
 	const int fixnumHeader = (*ppToken)[0];
-	int fixnum = -1;
+	int retFixnum = -1;
 
 	switch (fixnumHeader)
 	{
 	case 0x00: // 0
-		fixnum = fixnumHeader;
+		retFixnum = fixnumHeader;
 		(*ppToken) += 1;
 		break;
 	case 0x01: // unsigned [0x7B, 0xFF]
-		fixnum = (*ppToken)[1];
+		retFixnum = (*ppToken)[1];
 		(*ppToken) += 2;
 		break;
 	case 0x02: // unsigned [0x0100, 0xFFFF]
-		fixnum = (*ppToken)[1] | (*ppToken)[2] << 8;
+		retFixnum = (*ppToken)[1] | (*ppToken)[2] << 8;
 		(*ppToken) += 3;
 		break;
 	case 0x03: // unsigned [0x010000, 0xFFFFFF]
-		fixnum = (*ppToken)[1] | (*ppToken)[2] << 8 | (*ppToken)[3] << 16;
+		retFixnum = (*ppToken)[1] | (*ppToken)[2] << 8 | (*ppToken)[3] << 16;
 		(*ppToken) += 4;
 		break;
 	case 0x04: // unsigned [0x01000000, 0x3FFFFFFF]
 		__fallthrough;
 
 	case 0xFC: // signed [0xC0000000, 0xFEFFFFFF] => [-1073741824, -16777217]
-		fixnum = (*ppToken)[1] | (*ppToken)[2] << 8 | (*ppToken)[3] << 16 | (*ppToken)[4] << 24;
+		retFixnum = (*ppToken)[1] | (*ppToken)[2] << 8 | (*ppToken)[3] << 16 | (*ppToken)[4] << 24;
 		(*ppToken) += 5;
 		break;
 	case 0xFF: // signed [0x00, 0x84] => [-256, -124]
-		fixnum = ~(*ppToken)[1];
-		fixnum &= 0x000000ff;
-		fixnum = -(fixnum + 1);
+		retFixnum = ~(*ppToken)[1];
+		retFixnum &= 0x000000ff;
+		retFixnum = -(retFixnum + 1);
 		(*ppToken) += 2;
 		break;
 	case 0xFE: // signed [0x0000, 0xFEFF] => [-65536, -257]
-		fixnum = (*ppToken)[1] | (*ppToken)[2] << 8;
-		fixnum = ~fixnum;
-		fixnum &= 0x0000ffff;
-		fixnum = -(fixnum + 1);
+		retFixnum = (*ppToken)[1] | (*ppToken)[2] << 8;
+		retFixnum = ~retFixnum;
+		retFixnum &= 0x0000ffff;
+		retFixnum = -(retFixnum + 1);
 		(*ppToken) += 3;
 		break;
 	case 0xFD: // signed [0x000000, 0xFEFFFF] => [-16777216, -65537]
-		fixnum = (*ppToken)[1] | (*ppToken)[2] << 8 | (*ppToken)[3] << 16;
-		fixnum = ~fixnum;
-		fixnum &= 0x00ffffff;
-		fixnum = -(fixnum + 1);
+		retFixnum = (*ppToken)[1] | (*ppToken)[2] << 8 | (*ppToken)[3] << 16;
+		retFixnum = ~retFixnum;
+		retFixnum &= 0x00ffffff;
+		retFixnum = -(retFixnum + 1);
 		(*ppToken) += 4;
 		break;
 	default:
 		if (fixnumHeader >= 0x06 && fixnumHeader <= 0x7F)
 		{
-			fixnum = static_cast<char>(fixnumHeader - 0x05); // unsigned [0x01, 0x7A]
+			retFixnum = static_cast<char>(fixnumHeader - 0x05); // unsigned [0x01, 0x7A]
 			(*ppToken) += 1;
 		}
 		else if (fixnumHeader >= 0x80 && fixnumHeader <= 0xFA) 
 		{
-			fixnum = static_cast<char>(fixnumHeader + 0x05); // signed [0x85, 0xFF] => [-123, -1]
+			retFixnum = static_cast<char>(fixnumHeader + 0x05); // signed [0x85, 0xFF] => [-123, -1]
 			(*ppToken) += 1;
 		}
 		else
 		{
+			assert(false);
 			return false;
 		}
 		break;
 	}
 
-	*outVal = fixnum;
+	*outVal = retFixnum;
 
 	return true;
 }
@@ -180,9 +181,11 @@ int wmain(const int argc, const wchar_t* argv[])
 	//OpenFile(L"08_Fixnum_-65535.rxdata");
 	//OpenFile(L"08_Fixnum_-65536.rxdata");
 	//OpenFile(L"08_Fixnum_-65537.rxdata");
-	OpenFile(L"08_Fixnum_-16777215.rxdata");
-	OpenFile(L"08_Fixnum_-16777216.rxdata");
-	OpenFile(L"08_Fixnum_-16777217.rxdata");
+	//OpenFile(L"08_Fixnum_-16777215.rxdata");
+	//OpenFile(L"08_Fixnum_-16777216.rxdata");
+	//OpenFile(L"08_Fixnum_-16777217.rxdata");
+	//OpenFile(L"08_Fixnum_-1073741824.rxdata");
+	OpenFile(L"08_Fixnum_-1073741825.rxdata");
 
 	return 0;
 }
