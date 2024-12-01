7261fd932ec35bb8c58f833c7a44b63afd1f97ec
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2024-12-02 00:42:21 +0900

    Update Main.cpp

--- rgss_rxdata_parser/Main.cpp
+++ rgss_rxdata_parser/Main.cpp
@@ -1,18 +1,19 @@
-#include <cstdio>
+﻿#include <cstdio>
 #include <cassert>
 
 #include "eRubyTokens.h"
 
-void OpenFile(const wchar_t* const pWcsfileName);
+bool ReadBytes(const wchar_t* const pWcsfileName, unsigned char** ppOutData, unsigned int* pDataSize);
 bool Parse(const unsigned char* const paBuf, const unsigned int bufSize);
 
-void OpenFile(const wchar_t* const pWcsfileName)
+bool ReadBytes(const wchar_t* const pWcsfileName, unsigned char** ppOutData, unsigned int* pDataSize)
 {
 	FILE* pFile = nullptr;
 	errno_t err;
 	unsigned int bufSize;
 	unsigned int readByte;
 	unsigned char* paBuf = nullptr;
+	bool bSuccess = false;
 
 	do
 	{
@@ -33,16 +34,20 @@ void OpenFile(const wchar_t* const pWcsfileName)
 
 		paBuf = new unsigned char[bufSize];
 		readByte = (unsigned int)fread_s(paBuf, bufSize, sizeof(unsigned char), bufSize, pFile);
-		fclose(pFile);
 
-		assert(readByte > 0);
-		assert(readByte == bufSize);
+		bSuccess = true;
 
-		Parse(paBuf, bufSize);
-
-		delete[] paBuf;
+		*ppOutData = paBuf;
+		*pDataSize = readByte;
 
 	} while (0);
+
+	if (pFile != nullptr)
+	{
+		fclose(pFile);
+	}
+
+	return bSuccess;
 }
 
 bool ProcessFixnum(const unsigned char** ppToken, int* outVal)
@@ -58,14 +63,17 @@ bool ProcessFixnum(const unsigned char** ppToken, int* outVal)
 		retFixnum = fixnumHeader;
 		(*ppToken) += 1;
 		break;
+
 	case 0x01: // unsigned [0x7B, 0xFF]
 		retFixnum = (*ppToken)[1];
 		(*ppToken) += 2;
 		break;
+
 	case 0x02: // unsigned [0x0100, 0xFFFF]
 		retFixnum = (*ppToken)[1] | (*ppToken)[2] << 8;
 		(*ppToken) += 3;
 		break;
+
 	case 0x03: // unsigned [0x010000, 0xFFFFFF]
 		retFixnum = (*ppToken)[1] | (*ppToken)[2] << 8 | (*ppToken)[3] << 16;
 		(*ppToken) += 4;
@@ -77,12 +85,14 @@ bool ProcessFixnum(const unsigned char** ppToken, int* outVal)
 		retFixnum = (*ppToken)[1] | (*ppToken)[2] << 8 | (*ppToken)[3] << 16 | (*ppToken)[4] << 24;
 		(*ppToken) += 5;
 		break;
+
 	case 0xFF: // signed [0x00, 0x84] => [-256, -124]
 		retFixnum = ~(*ppToken)[1];
 		retFixnum &= 0x000000ff;
 		retFixnum = -(retFixnum + 1);
 		(*ppToken) += 2;
 		break;
+
 	case 0xFE: // signed [0x0000, 0xFEFF] => [-65536, -257]
 		retFixnum = (*ppToken)[1] | (*ppToken)[2] << 8;
 		retFixnum = ~retFixnum;
@@ -90,6 +100,7 @@ bool ProcessFixnum(const unsigned char** ppToken, int* outVal)
 		retFixnum = -(retFixnum + 1);
 		(*ppToken) += 3;
 		break;
+
 	case 0xFD: // signed [0x000000, 0xFEFFFF] => [-16777216, -65537]
 		retFixnum = (*ppToken)[1] | (*ppToken)[2] << 8 | (*ppToken)[3] << 16;
 		retFixnum = ~retFixnum;
@@ -97,6 +108,7 @@ bool ProcessFixnum(const unsigned char** ppToken, int* outVal)
 		retFixnum = -(retFixnum + 1);
 		(*ppToken) += 4;
 		break;
+
 	default:
 		if (fixnumHeader >= 0x06 && fixnumHeader <= 0x7F)
 		{
@@ -110,7 +122,6 @@ bool ProcessFixnum(const unsigned char** ppToken, int* outVal)
 		}
 		else
 		{
-			assert(false);
 			return false;
 		}
 		break;
@@ -134,22 +145,27 @@ bool ProcessStringUTF8(const unsigned char** ppToken)
 	case 0x00: // zero length
 		(*ppToken)++;
 		break;
+
 	case 0x01:
 		utf8ByteLength = (*ppToken)[1];
 		(*ppToken) += 2;
 		break;
+
 	case 0x02:
 		utf8ByteLength = (*ppToken)[1] | (*ppToken)[2] << 8;
 		(*ppToken) += 3;
 		break;
+
 	case 0x03:
 		utf8ByteLength = (*ppToken)[1] | (*ppToken)[2] << 8 | (*ppToken)[3] << 16;
 		(*ppToken) += 4;
 		break;
+
 	case 0x04:
 		utf8ByteLength = (*ppToken)[1] | (*ppToken)[2] << 8 | (*ppToken)[3] << 16 | (*ppToken)[4] << 24;
 		(*ppToken) += 5;
 		break;
+
 	default:
 		if (stringLengthHeader >= 0x05 && stringLengthHeader <= 0xff)
 		{
@@ -159,7 +175,6 @@ bool ProcessStringUTF8(const unsigned char** ppToken)
 		}
 		else
 		{
-			assert(false);
 			return false;
 		}
 	}
@@ -199,18 +214,23 @@ bool Parse(const unsigned char* const paBuf, const unsigned int bufSize)
 		case TYPE_NIL:
 			++pToken;
 			break;
+
 		case TYPE_TRUE:
 			++pToken;
 			break;
+
 		case TYPE_FALSE:
 			++pToken;
 			break;
+
 		case TYPE_FIXNUM:
 			ProcessFixnum(&pToken, &val);
 			break;
+
 		case TYPE_STRING:
 			ProcessStringUTF8(&pToken);
 			break;
+
 		default:
 			break;
 		}
@@ -221,24 +241,32 @@ bool Parse(const unsigned char* const paBuf, const unsigned int bufSize)
 
 int wmain(const int argc, const wchar_t* argv[])
 {
-	//OpenFile(L"06_Fixnum_16777216.rxdata");
-	//OpenFile(L"06_Fixnum_16777217.rxdata");
-	//OpenFile(L"07_Fixnum_-1.rxdata");
-	//OpenFile(L"07_Fixnum_-123.rxdata");
-	//OpenFile(L"07_Fixnum_-124.rxdata");
-	//OpenFile(L"07_Fixnum_-256.rxdata");
-	//OpenFile(L"07_Fixnum_-257.rxdata");
-	//OpenFile(L"08_Fixnum_-65534.rxdata");
-	//OpenFile(L"08_Fixnum_-65535.rxdata");
-	//OpenFile(L"08_Fixnum_-65536.rxdata");
-	//OpenFile(L"08_Fixnum_-65537.rxdata");
-	//OpenFile(L"08_Fixnum_-16777215.rxdata");
-	//OpenFile(L"08_Fixnum_-16777216.rxdata");
-	//OpenFile(L"08_Fixnum_-16777217.rxdata");
-	//OpenFile(L"08_Fixnum_-1073741824.rxdata");
-	//OpenFile(L"08_Fixnum_-1073741825.rxdata");
-	//OpenFile(L"marshals/String_abcd.rxdata");
-	OpenFile(L"marshals/String_0123가나다라ABCD.rxdata");
+	unsigned char* paBuf;
+	unsigned int bufSize;
+
+	//ReadBytes(L"06_Fixnum_16777216.rxdata");
+	//ReadBytes(L"06_Fixnum_16777217.rxdata");
+	//ReadBytes(L"07_Fixnum_-1.rxdata");
+	//ReadBytes(L"07_Fixnum_-123.rxdata");
+	//ReadBytes(L"07_Fixnum_-124.rxdata");
+	//ReadBytes(L"07_Fixnum_-256.rxdata");
+	//ReadBytes(L"07_Fixnum_-257.rxdata");
+	//ReadBytes(L"08_Fixnum_-65534.rxdata");
+	//ReadBytes(L"08_Fixnum_-65535.rxdata");
+	//ReadBytes(L"08_Fixnum_-65536.rxdata");
+	//ReadBytes(L"08_Fixnum_-65537.rxdata");
+	//ReadBytes(L"08_Fixnum_-16777215.rxdata");
+	//ReadBytes(L"08_Fixnum_-16777216.rxdata");
+	//ReadBytes(L"08_Fixnum_-16777217.rxdata");
+	//ReadBytes(L"08_Fixnum_-1073741824.rxdata");
+	//ReadBytes(L"08_Fixnum_-1073741825.rxdata");
+	//ReadBytes(L"marshals/String_abcd.rxdata");
+	
+	if (ReadBytes(L"marshals/String_0123가나다라ABCD.rxdata", &paBuf, &bufSize))
+	{
+		Parse(paBuf, bufSize);
+	}
+
 
 	return 0;
 }
