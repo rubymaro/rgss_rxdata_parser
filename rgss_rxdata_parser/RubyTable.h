824f0c7142623b09f68db64b1c0e08a4834f4533
Jubin Park <15997924+jubin-park@users.noreply.github.com>
2024-12-23 11:37:14 +0900

    Update RubyTable.h

--- rgss_rxdata_parser/RubyTable.h
+++ rgss_rxdata_parser/RubyTable.h
@@ -9,5 +9,8 @@ struct RubyTable final
 	uint32_t SizeY;
 	uint32_t SizeZ;
 	uint32_t ElementCount;
+#pragma warning(push)
+#pragma warning(disable : 4200)
 	int16_t PElement[0];
+#pragma warning(pop)
 };
